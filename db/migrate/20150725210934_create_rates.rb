78f994acd54158a2b513db589163909caef83592
Anthony Sosa <anjrsosa@gmail.com>
2015-07-25 21:45:26 +0000

    migrate

--- db/migrate/20150725210934_create_rates.rb
+++ db/migrate/20150725210934_create_rates.rb
@@ -2,7 +2,7 @@ class CreateRates < ActiveRecord::Migration
   def change
     create_table :rates do |t|
       t.string :name
-      t.double :price
+      t.float :price
       t.references :district, index: true, foreign_key: true
       t.references :district, index: true, foreign_key: true
 
