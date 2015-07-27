0706c49a3458de2f6cf07260b447e50247a9aa25
Richard Checnes <richardchecnes@hotmail.com>
2015-07-27 21:04:05 +0000

    se agregó un input en servicios

--- app/controllers/service_drivers_controller.rb
+++ app/controllers/service_drivers_controller.rb
@@ -1,4 +1,9 @@
 class ServiceDriversController < ApplicationController
+  before_action :set_service, only: [:show, :edit, :update, :destroy]
+  layout "administrator"
+  
   def index
+    @title = "Servicios por Atender"
+    @services = Service.all
   end
 end
