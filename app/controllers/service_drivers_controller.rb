2f1d987428515c05a730531b4ede241b66d0af43
Edwin Montoya <system.edwin@hotmail.com>
2015-07-28 02:54:21 +0000

    Test

--- app/controllers/service_drivers_controller.rb
+++ app/controllers/service_drivers_controller.rb
@@ -1,29 +1,29 @@
 class ServiceDriversController < ApplicationController
-  before_action :set_service, only: [:accept]
+  before_action :set_service, only: [:accept, :reject,:start,:terminate]
  layout "administrator"
   
-  
-  
-  
-  
-  
-  
   def index
-    @chofer =1
-    @title = "Servicios por Atender"
-    @services = Service.find(@chofer)
-  end
-  
-  def Aceptar
-   
-    @title = "Servicios por Atender"
-    @services = Service.find_by(driver_id: '1')
-   
+
+    @chofer=1
+    @title = "Servicios Asignados"
+    @services = Service.where(driver_id:@chofer)
+
   end
   
   def accept
-   
-       
+
+    
   end
+  
+  def reject
+  end 
+    
+  def start
+  end
+  
+  def terminate
+  end
+
+
   
 end 
