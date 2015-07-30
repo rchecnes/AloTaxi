270fdbf7d15bb5acedd2e9d156f84414ea568be1
Anthony Sosa <anjrsosa@gmail.com>
2015-07-30 22:30:45 +0000

    Assign vehicle and driver a services

--- db/migrate/20150728013922_remove_fields_from_service.rb
+++ db/migrate/20150728013922_remove_fields_from_service.rb
@@ -1,9 +1,9 @@
 class RemoveFieldsFromService < ActiveRecord::Migration
   def change
-    remove_column :services, :customer_id
-    remove_column :services, :driver_id
-    remove_column :services, :to_address_id
-    remove_column :services, :from_address_id
+    # remove_column :services, :customer_id
+    # remove_column :services, :driver_id
+    # remove_column :services, :to_address_id
+    # remove_column :services, :from_address_id
   end
 end
  
