da282debe235a52441e0ad7374f77f58685c9ebf
Richard Checnes <richardchecnes@hotmail.com>
2015-08-01 20:33:48 +0000

    se quito la validacion en servicios

--- app/models/service.rb
+++ app/models/service.rb
@@ -10,11 +10,10 @@ class Service < ActiveRecord::Base
   belongs_to :from_address ,  :class_name  =>  "Address" 
   belongs_to :to_address ,  :class_name  =>  "Address"
   
-  validates :scheduled_to, presence: true
-  validates :requested_seats, presence: true
-  validates :vehicle_type, presence: true
-  validates :payment_type, presence: true
-  validates :price, presence: true
-  validates :requested_seats, presence: true
-  
+  #validates :scheduled_to, presence: true
+  #validates :requested_seats, presence: true
+  #validates :vehicle_type, presence: true
+  #validates :payment_type, presence: true
+  #validates :price, presence: true
+  #validates :requested_seats, presence: true
 end
